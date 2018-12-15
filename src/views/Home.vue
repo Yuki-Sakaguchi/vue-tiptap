f235c476ef315ce385f2b114b568972af4e3948c
Yuki-Sakaguchi <sakaguchi.ba55.0ne@gmail.com>
2018-12-16 00:12:05 +0900

    [mod]

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,79 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div>
+    <editor-menu-bar :editor="editor">
+      <div slot-scope="{ commands, isActive }"></div>
+    </editor-menu-bar>
+    <editor-content :editor="editor" />
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+<script>
+import { Editor, EditorContent, EditorMenuBar } from 'tiptap'
+import {
+  Blockquote,
+  CodeBlock,
+  HardBreak,
+  Heading,
+  OrderedList,
+  BulletList,
+  ListItem,
+  TodoItem,
+  TodoList,
+  Bold,
+  Code,
+  Italic,
+  Link,
+  Strike,
+  Underline,
+  History,
+} from 'tiptap-extensions'
 
-@Component({
+export default {
   components: {
-    HelloWorld,
+    EditorMenuBar,
+    EditorContent,
   },
-})
-export default class Home extends Vue {}
+  data() {
+    return {
+      editor: new Editor({
+        extensions: [
+          new Blockquote(),
+          new CodeBlock(),
+          new HardBreak(),
+          new Heading({ levels: [1, 2, 3] }),
+          new BulletList(),
+          new OrderedList(),
+          new ListItem(),
+          new TodoItem(),
+          new TodoList(),
+          new Bold(),
+          new Code(),
+          new Italic(),
+          new Link(),
+          new Strike(),
+          new Underline(),
+          new History(),
+        ],
+        content: `
+          <h1>Vueでリッチテキストを作ってみる</h1>
+          <p>vueのtiptapというライブラリを使っています</p>
+        `,
+      }),
+    }
+  },
+  beforeDestroy() {
+    this.editor.destroy()
+  },
+}
 </script>
+
+
+
+<style>
+.ProseMirror {
+  outline: none;
+}
+.ProseMirror:focus {
+  outline: none;
+}
+</style>
